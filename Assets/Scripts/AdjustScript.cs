ab931091651f5db28fbeec3f403f9ba5ba9fa71d
Mizuta Hiro <mzthr78@gmail.com>
2019-11-01 16:52:25 +0900

    trial placement

---
+++ Assets/Scripts/AdjustScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustScript : MonoBehaviour
+{
+    public Text InfoText;
+
+    bool selected = false;
+
+    static bool Selected
+    {
+        get
+        {
+            return Selected;
+        }
+        set
+        {
+            Selected = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
