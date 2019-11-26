3f82a9406ea616edc3a177813a508ae2d7272b0d
Mizuta Hiro <mzthr78@gmail.com>
2019-11-26 23:42:53 +0900

    modify stage etc...

---
+++ Assets/Scripts/Title/TextFuwa.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFuwa : MonoBehaviour
+{
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+
+        StartCoroutine(Fuwa());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Fuwa()
+    {
+        yield return null;
+    }
+}
